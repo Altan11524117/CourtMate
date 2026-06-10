75a74d3f4780657fe1759f32c179c3c96b45a19e
Altan <altan@odev.com>
2026-06-10 13:12:35 +0300

    fix: Disable SSL for PostgreSQL connection in development
    - Change sslmode=require to sslmode=disable in DSN
    - Fixes connection errors for local development with Docker
    - Note: Re-enable sslmode=require in production

--- backend/config/database.go
+++ backend/config/database.go
@@ -15,7 +15,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=UTC",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
